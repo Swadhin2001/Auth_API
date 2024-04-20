85d7fb04798e85292836e1d20650f37bb8b32b0c
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-04-20 12:23:42 +0530

    port added

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.use (bodyParser.urlencoded({ extended: true }));
 app.use (bodyParser.json());
